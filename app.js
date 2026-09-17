d96836e430d871003941a0a99b37e4a23a36c45d
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-09-16 22:43:12 -0700

    initial route shells

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 import express from "express";
 
 const app = express();
+
+// parse requests with Content-Type of application/json
+app.use(express.json());
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Kanban Board Running");
+});
+
+app.get("/tasks", (req, res) => {
+  res.send("Get list of existing tasks from db");
+});
+
+app.get("/tasks/:id", (req, res) => {
+  res.send("Get specific task from db");
+});
+
+app.post("/tasks", (req, res) => {
+  res.send("Add new task to db");
+});
+
+app.put("/tasks/:id", (req, res) => {
+  res.send("Update task state in db");
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  res.send("Delete task from db");
 });
 
 app.listen(port, () => {
