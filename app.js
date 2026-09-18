2f6f61e1cd50284b3d6c894ca6598b4a14723762
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-09-17 20:27:43 -0700

    update endpoints with sqlite queries

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import db from "./db.js";
 
 const app = express();
 
@@ -12,23 +13,47 @@ app.get("/", (req, res) => {
 });
 
 app.get("/tasks", (req, res) => {
-  res.send("Get list of existing tasks from db");
+  const tasks = db.prepare("SELECT * FROM tasks").all();
+
+  res.json(tasks);
 });
 
 app.get("/tasks/:id", (req, res) => {
-  res.send("Get specific task from db");
+  const { id } = req.params;
+  const task = db.prepare("SELECT * FROM tasks WHERE id = ?").get(id);
+  res.json(task);
 });
 
 app.post("/tasks", (req, res) => {
-  res.send("Add new task to db");
+  const { title, priority, due_date, column_name } = req.body;
+  const result = db
+    .prepare(
+      "INSERT INTO tasks (title, priority, due_date, column_name) VALUES (?, ?, ?, ?)",
+    )
+    .run(title, priority, due_date, column_name);
+
+  const newTask = db
+    .prepare("SELECT * FROM tasks WHERE id = ?")
+    .get(result.lastInsertRowid);
+  res.status(201).json(newTask);
 });
 
-app.put("/tasks/:id", (req, res) => {
-  res.send("Update task state in db");
+app.patch("/tasks/:id", (req, res) => {
+  const { column_name } = req.body;
+  const { id } = req.params;
+
+  const result = db
+    .prepare("UPDATE tasks SET column_name = ? WHERE id = ?")
+    .run(column_name, id);
+  res.json(result);
 });
 
 app.delete("/tasks/:id", (req, res) => {
-  res.send("Delete task from db");
+  const { id } = req.params;
+  const result = db
+    .prepare("DELETE FROM tasks WHERE id = ?")
+    .run(req.params.id);
+  res.json(result);
 });
 
 app.listen(port, () => {
---
+++ db.js
@@ -0,0 +1,20 @@
+/*
+db.js
+Handles creating and opening the kanban database
+*/
+
+import Database from "better-sqlite3";
+
+const db = new Database("kanban.db");
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    priority TEXT NOT NULL,
+    due_date TEXT,
+    column_name TEXT NOT NULL DEFAULT 'todo'
+  )
+`);
+
+export default db;
